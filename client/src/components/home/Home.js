0428e91a87577f0944317a3ac28780cd44b59832
Nirmal <nirmalraj1025@gmail.com>
2023-08-24 23:45:42 +0530

    first commit

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -37,7 +37,7 @@ function Home() {
     <div className="w-100">
       <Helmet>
         <meta charSet="utf-8" />
-        <link rel="icon" href="../../../public/favicon.jpg" />
+        <link rel="icon" href="./favicon.jpg" />
         <title>Awesome Crackers</title>
         <link rel="canonical" href="http://mysite.com/example" />
       </Helmet>
